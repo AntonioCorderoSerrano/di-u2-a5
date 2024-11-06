bd51aab336304a4335d10c3f54f568d31ea3409f
Antonio Cordero Serrano <antoniocorderoserrano@gmail.com>
2024-11-06 09:46:28 +0100

    Finalizado despues de corregir

--- src/app/Clock.js
+++ src/app/Clock.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './styles.css'
 
 export default function Clock({ time }) {
   const hours = time.getHours();
-  const timeOfDay = (hours >= 0 && hours <= 6) ? 'night' : 'day';
+  let timeOfDay = (hours >= 0 && hours <= 6) ? 'night' : 'day';
 
   return (
     <h1 id="time" className={timeOfDay}>
